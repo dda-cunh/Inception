9fdc31ee03d01d6828eb1eba548f7bb67a57135e
dda-cunh <dmoutinh@student.42lisboa.com>
2024-12-29 22:00:52 +0000

    mariadb is oky

--- Makefile
+++ Makefile
@@ -5,14 +5,20 @@ COMPOSE_FILE_PATH	=	./srcs/docker-compose.yml
 
 COMPOSE				=	${DOCKER_COMPOSE} -f ${COMPOSE_FILE_PATH}
 
-up:
-		${COMPOSE} up -d --build
+up:	
+			${COMPOSE} up -d --build
+
+no_cache:	fclean
+			${COMPOSE} build --no-cache
+
+logs:
+			${COMPOSE} logs
 
 down:
-		${COMPOSE} down
+			${COMPOSE} down
 
 clean:
-		${COMPOSE} down --rmi all
+			${COMPOSE} down --rmi all
 
 fclean:
-		${COMPOSE} down --rmi all --volumes
+			${COMPOSE} down --rmi all  --volumes
---
+++ srcs/mariadb/config/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! [ -f /service/.mysql_is_setup ]; then
+
+	service mariadb start
+
+	mysql -u root -e "
+					DELETE FROM mysql.user WHERE User='' AND Host='localhost';
+					DELETE FROM mysql.user WHERE User='' AND Host='$(hostname)';
+					DROP DATABASE IF EXISTS test;
+					ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';
+					CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};
+					CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';
+					GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%';
+					FLUSH PRIVILEGES;
+					SHUTDOWN;
+				"
+
+	touch /service/.mysql_is_setup
+fi
+
+mysqld_safe
